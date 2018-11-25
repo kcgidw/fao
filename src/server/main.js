299bf0b55e40c11e4df4b67414e97f70871efd97
kcgidw <kc.gideonw@gmail.com>
2018-11-25 15:42:52 -0800

    snychronous prompt loading

--- src/server/main.js
+++ src/server/main.js
@@ -11,6 +11,11 @@ const io = SocketIO(httpServer);
 require('./lobby').handleSocketIO(io);
 
 app.use(express.static(path.resolve(__dirname, '..', 'public')));
-httpServer.listen(port, function() {
-	console.log(`httpServer listening on port ${port}`);
+
+const Prompts = require('./prompts');
+Prompts.getPrompts.then((outputs) => {
+	console.log(`Prompts loaded. Counted ${outputs.length} prompts`);
+	httpServer.listen(port, function() {
+		console.log(`httpServer listening on port ${port}`);
+	});
 });
--- src/server/prompts.js
+++ src/server/prompts.js
@@ -3,20 +3,24 @@ const fs = require('fs');
 const parse = require('csv-parse');
 const Util = require('../common/util');
 
-var prompts;
-
 const filename = path.resolve(__dirname, 'prompts.csv');
 //TODO synchronous
-fs.readFile(filename, function(err, fileData) {
-	parse(fileData, {columns: true, trim: true}, function(err, output) {
-		// console.log(output);
-		prompts = output;
+
+var prompts;
+var getPrompts = new Promise(function(resolve, reject) {
+	fs.readFile(filename, function(err, fileData) {
+		parse(fileData, {columns: true, trim: true}, function(err, output) {
+			if(err) {
+				throw err;
+				// reject(err);
+			} else {
+				prompts = output;
+				resolve(output);
+			}
+		});
 	});
 });
 
-function getPrompts() {
-	return prompts;
-}
 function getRandomPrompt() {
 	if(prompts === undefined) {
 		return undefined;
