c9becfc3b705118d80df2ebf7ca972ad2f28d30e
kcgidw <kc.gideonw@gmail.com>
2019-05-04 11:12:05 -0700

    Fix item alignment in player list dialog

--- src/public/js/player-statuses.vue
+++ src/public/js/player-statuses.vue
@@ -3,11 +3,9 @@
 		<h3>Players</h3>
 		<ul>
 			<li v-for="u in users" :key="'0'+u.name">
-				<span class="color-spot" :style="{color: color(u)}">
-					<svg class="feather">
-						<circle cx="12" cy="12" r="4" stroke="black" stroke-width="0" :fill="color(u)" />
-					</svg>
-				</span>
+				<svg class="feather color-spot" :style="{color: color(u)}">
+					<circle cx="12" cy="12" r="4" stroke="black" stroke-width="0" :fill="color(u)" />
+				</svg>
 				<span class="username" :style="{color: color(u)}">{{u.name}}</span>
 				<span class="grow-space"></span>
 				<span class="connectionStatus" :class="{tinytext: true, connected: u.connected, disconnected: !u.connected}">{{connectionStatusString(u)}}</span>
