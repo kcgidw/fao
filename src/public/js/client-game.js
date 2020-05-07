70a0d65e08e3d1e928e003f74ec54b8238c7f5bf
kcgidw <kc.gideonw@gmail.com>
2020-05-07 13:06:53 -0700

    Update player colors

--- src/public/js/client-game.js
+++ src/public/js/client-game.js
@@ -1,4 +1,4 @@
-import * as Color from './color';
+import * as Color from './playerColors';
 
 function generateClientGameState() {
 	return {
--- src/public/js/playerColors.js
+++ src/public/js/playerColors.js
@@ -1,19 +1,19 @@
 const HEX = {
-	MAGENTA: '#cc0088',
+	MAGENTA: '#bb0077',
 	RED: '#e50000',
 	ORANGE: '#f28900',
-	GOLD: '#ddbb00',
-	GREEN: '#3dbb00',
+	YELLOW: '#ddbb00',
+	GREEN: '#77bb00',
 	TEAL: '#00aaaa',
 	BLUE: '#0044cc',
-	PURPLE: '#9300dd',
+	PURPLE: '#9900ee',
 	PINK: '#ee77ff',
 	BROWN: '#a56729',
 };
 
 const ORDER = [
 	'RED',
-	'GOLD',
+	'YELLOW',
 	'GREEN',
 	'BLUE',
 	'PURPLE',
